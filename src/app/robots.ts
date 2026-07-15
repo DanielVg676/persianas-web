a76554c611c620bfd6b4220bdfea6e145d6ce02b
DanielVg676 <dv956543@gmail.com>
2026-07-15 11:47:17 -0600

    wadada

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { siteConfig } from "@/shared/constants/site";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
